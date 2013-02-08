2501b122e47341ec61136d960b990568ad030b2f
TatsuhiroYamada <yamada@moco.matlab.nitech.ac.jp>
2013-02-08 13:55:53 +0900

    register files

---
+++ emacs/emacs.el
@@ -0,0 +1,509 @@
+(defvar matlab:current-system
+  (nth 2 (split-string system-configuration "-")))
+
+;;; switch variables
+(defvar matlab:use-mew		nil) ;; set this t to use Mew
+(defvar matlab:use-wl		nil) ;; set this t to use Wanderlust
+
+;;;
+;;; Key customize
+;;;
+(global-set-key [(control h)]	'delete-backward-char) ;バックスペース
+(global-set-key [(meta g)]	'goto-line) 
+(global-set-key [(control space)] 'set-mark-command) 
+(global-set-key [?\S-\ ] 'toggle-input-method) ; shift-spaceで入力変換
+
+;; emacsの終了動作C-xC-cを無効化する
+;;  M-x kill-emacs で終了させる
+(global-unset-key "\C-x\C-c")
+
+
+;;;
+;;; Language
+;;;
+(set-language-environment "Japanese")
+
+;;; coding system
+(set-default-coding-systems	'utf-8)
+(set-buffer-file-coding-system	'utf-8)
+(set-terminal-coding-system	'utf-8)
+(setq default-file-name-coding-system 'utf-8)
+
+;;;
+;;; Japanese Input Method
+;;;
+;; Anthy 
+;;
+(when (string-match "linux" system-configuration)
+ (load-library "anthy")
+ (or 
+  (and (assoc "japanese-egg-anthy" input-method-alist)	; egg-anthy
+	(require 'egg)
+	(setq enable-double-n-syntax		t)
+	(setq egg-conversion-wrap-select	t
+	      egg-conversion-auto-candidate-menu 2)
+	(setq default-input-method "japanese-egg-anthy"))
+  (and (assoc "japanese-anthy" input-method-alist)	; anthy
+	(setq default-input-method 'japanese-anthy))))
+;;anthyのタイムアウト時間を短くする
+(load-library "anthy")
+(if (>= emacs-major-version 23)
+   (setq anthy-accept-timeout 1))  
+
+
+
+;;;elispプログラムを置くところへパスを通す
+(setq load-path (cons "~/elisp" load-path))
+
+
+;;;
+;;; highlight
+;;;
+(require 'font-lock)
+(global-font-lock-mode t)
+;リージョンハイライト
+(transient-mark-mode t)
+;括弧の対応付け
+(require 'paren)
+(show-paren-mode t)
+;;カーソルの行をハイライト
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "#333333"))
+    (((class color)
+      (background light))
+     (:background "light gray"))
+     (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+;;(setq hl-line-face 'underline) ;; under line 
+(global-hl-line-mode)
+;rubyで対応するendをハイライト
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
+
+
+;;;
+;;; TeX
+;;;
+(if (locate-library "auctex")
+    (load "auctex.el" nil t t))
+(setq TeX-japanese-process-input-coding-system  'japanese-iso-8bit
+      TeX-japanese-process-output-coding-system 'iso-2022-jp
+      LaTeX-version			"2e"
+      japanese-LaTeX-default-style	"jarticle"
+      TeX-default-mode			'japanese-latex-mode
+      TeX-force-default-mode		t
+      LaTeX-top-caption-list		'("table" "tabular")
+      TeX-command-default		"pLaTeX"
+      TeX-parse-self			t
+      japanese-LaTeX-command-default	"pLaTeX"
+      LaTeX-float			"tn"
+      LaTeX-figure-label		"fig:"
+      LaTeX-table-label			"tab:"
+      LaTeX-section-label		"sec:")
+(eval-after-load "auctex"
+  '(when window-system
+     (require 'font-latex)))
+;;; bibtex
+(setq bib-bibtex-env-variable		"TEXMFHOME")
+(autoload 'turn-on-bib-cite "bib-cite")
+(add-hook 'LaTeX-mode-hook 'turn-on-bib-cite)
+;;; reftex
+(setq reftex-texpath-environment-variables	'("TEXMFHOME")
+      reftex-bibpath-environment-variables	'("~/texmf//")
+      reftex-plug-into-AUCTeX			t
+      reftex-label-alist
+      '(("figure"       ?F "fig:" "\\figref{%s}" caption nil)
+	("figure*"      ?F nil nil caption)
+	("table"        ?T "tab:" "\\tabref{%s}" caption nil)
+	("table*"       ?T nil nil caption)))
+(autoload 'reftex-mode     "reftex" "RefTeX Minor Mode" t)
+(autoload 'turn-on-reftex  "reftex" "RefTeX Minor Mode" nil)
+(autoload 'reftex-citation "reftex-cite" "Make citation" nil)
+(autoload 'reftex-index-phrase-mode "reftex-index" "Phrase mode" t)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'LaTeX-mode-hook 'outline-minor-mode)   ; with AUCTeX LaTeX mode
+(add-hook 'LaTeX-mode-hook 'auto-fill-mode)   ; with AUCTeX LaTeX mode
+;;;ビューソフトをpxdviにする
+(setq TeX-output-view-style '(("^dvi$" "." "pxdvi '%d'")))
+
+
+;;;
+;;; Mouse Wheel
+;;;
+(mwheel-install)
+(setq mouse-wheel-follow-mouse t)
+
+;;;
+;;; Message User Agent
+;;;
+;; Mew
+;;
+(when matlab:use-mew
+  (autoload 'mew	"mew" nil t)
+  (autoload 'mew-send	"mew" nil t)
+  (if (file-exists-p (expand-file-name "~/.mew.el"))
+    (load (expand-file-name "~/.mew.el") nil t nil))
+  )
+
+;; Wanderlust
+;;
+(when matlab:use-wl
+  (autoload 'wl		"wl" "wanderlust" t)
+  (setq smtp-local-domain	"matlab.nitech.ac.jp"
+	wl-smtp-posting-server	"mail"			;; smtp server
+	elmo-imap4-default-server	"mail"		;; imap server
+	wl-biff-check-folder-list	'("%inbox")
+	wl-from			(concat (user-login-name) "@matlab.nitech.ac.jp")
+	wl-message-id-domain	"matlab.nitech.ac.jp"
+	wl-use-ldap		t
+	wl-ldap-server		"mats.matlab.nitech.ac.jp"
+	wl-ldap-base		"dc=matlab,dc=nitech,dc=ac,dc=jp"
+	wl-draft-reply-buffer-style 'keep
+	wl-draft-folder		"..Draft"
+	wl-trash-folder		"..Trash"
+	wl-queue-folder		"..queue"
+	wl-interactive-send	t)
+  )
+(when (string-match "solaris" system-configuration)
+	(setq ldap-search-program-arguments '("-L" "-T")))
+
+;;; SEMI (for wl etc)
+(setq mail-self-blind		t
+      mime-edit-split-message   nil
+      mime-header-accept-quoted-encoded-words t
+      mime-view-ignored-field-list
+      '(".*Received:" ".*Path:" ".*Id:" "^References:"
+        "^Replied:" "^Errors-To:" "^Lines:" ".*Host:" "^Xref:"
+        "^Precedence:" "^Status:" "^X-VM-.*:"
+        "^Mail-Reply-To:" "^Mail-Followup-To:" "^In-Reply-To:" "^X-*"
+        "^Posted:" "^Mailing-List:" "^Delivered-To:" "^Date:"
+        "^Content-Transfer-Encoding:" "^Message-Id:"
+        "^List-Help:" "^List-Post:" "^List-Subscribe:"
+        "^List-Unsubscribe:" "^List-Archive:"
+        )
+      mime-view-visible-field-list
+      '("^Dnas.*:" "^Message-Id:"
+        "^User-Agent:" "^X-Mailer:" "^X-Newsreader:" "^X-MimeOLE:" "^X-Emacs:"
+        "^Content-Type:" "^X-Dispatcher:" "^X-Face:" "^X-Face-.*:"))
+
+;;;
+;;; tramp
+;;;
+;; (if (locate-library "tramp")
+;;    (require 'tramp))
+
+;;;
+;;; emacsclient
+;;;
+(server-start)
+
+
+;; ;;;elispの中のファイルを読み込む
+;; (require 'dabbrev-expand-multiple)
+;; (global-set-key "\M-/" 'dabbrev-expand-multiple)
+;; (setq outline-minor-mode-prefix "\M-o")
+
+
+;;;
+;;;ツールバーを非表示
+;;;
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
+
+
+;;
+;;Emacsのバージョンが23のときテーマ適用
+;;
+(if (>= emacs-major-version 23)
+      (progn
+	(setq load-path (cons "~/elisp/color-theme-6.6.0" load-path))
+	(require 'color-theme)
+	(color-theme-initialize)
+	(color-theme-dark-laptop)
+	)
+)
+
+;起動時にscratchバッファ
+(setq inhibit-startup-screen t)
+
+;;compizとか使用時のemacsの透過具合
+(modify-all-frames-parameters 
+ (list (cons 'alpha '(90 75 40 30))))
+
+
+; emacs23 用フォンと設定
+(cond ((string-match "^23\." emacs-version)
+       (cond (window-system
+ ;            (set-default-font "DejaVu LGC Sans Mono-10:style=Bold")
+            (set-default-font "Source Code Pro-10")
+	      ;(set-default-font "M+1P+IPAG-11")
+              (set-fontset-font (frame-parameter nil 'font)
+	      	  'japanese-jisx0208
+                  '("うずらフォント" . "unicode-bmp"))
+))))
+;; 新規フレームのデフォルト設定
+;;フォントの設定より後ろに書く必要アリ
+(modify-all-frames-parameters (list (assq 'font (frame-parameters))))
+
+
+;;
+;; iswitchb-mode
+;;
+(iswitchb-mode 1)
+(setq read-buffer-function 'iswitchb-read-buffer)
+(setq iswitchb-regexp nil)
+(setq iswitchb-prompt-newbuffer nil)
+(setq iswitchb-buffer-ignore
+      '("^ "
+	"\*GNU Emacs\*"
+	"\*Buffer List\*"
+	"\*Messages\*"
+	"\*Compile-Log\*"
+	"\*Completions\*"
+	"\*auto-install"
+	"\*ENB All notes\*"
+	"\*Evernote"
+	"\*trace of SMTP session to mail\*"
+	"\*elmo-split\*"
+	"\*GTAGS"
+	"\.ipa"))
+
+
+;;
+;;バックアップファイルを~/.emacsbacksに吐かせる
+(setq make-backup-files t)
+(setq backup-directory "~/.emacsbacks")
+(if (and (boundp 'backup-directory)
+         (not (fboundp 'make-backup-file-name-original)))
+    (progn
+      (fset 'make-backup-file-name-original
+            (symbol-function 'make-backup-file-name))
+      (defun make-backup-file-name (filename)
+        (if (and (file-exists-p (expand-file-name backup-directory))
+                 (file-directory-p (expand-file-name backup-directory)))
+            (concat (expand-file-name backup-directory) 
+                    "/" (file-name-nondirectory filename))
+          (make-backup-file-name-original filename)))))
+
+;;
+;;ruby
+;;
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+
+;;emacs起動時にhs-minor-modeを自動起動
+;;rubyやlispの時はまだ未対応
+;; (add-hook 'c-mode-common-hook
+;; '(lambda()
+;; (hs-minor-mode 1))) 
+(load-library "hideshow")
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+(add-hook 'java-mode-hook 'hs-minor-mode)
+(add-hook 'perl-mode-hook 'hs-minor-mode)
+(add-hook 'php-mode-hook 'hs-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
+;;rubyに対応させる
+(add-hook 'ruby-mode-hook
+          '(lambda()
+            (hs-minor-mode 1)))
+(let ((ruby-mode-hs-info
+       '(ruby-mode
+          "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
+          "end"
+          "#"
+          ruby-move-to-block
+          nil)))
+  (if (not (member ruby-mode-hs-info hs-special-modes-alist))
+      (setq hs-special-modes-alist
+            (cons ruby-mode-hs-info hs-special-modes-alist))))
+;; 隠した(hide)行は、isearchの対象にしない(outlineモードにも影響)
+(setq search-invisible nil) ;; 「'open」or「t」
+;; hs-minor-modeのキーバインド達
+(define-key hs-minor-mode-map (kbd "C-c C-t") 'hs-toggle-hiding)
+(define-key hs-minor-mode-map (kbd "C-c C-a C-h") 'hs-hide-all)
+(define-key hs-minor-mode-map (kbd "C-c C-a C-s") 'hs-show-all)
+
+
+;;タグジャンプする際にTAGSファイルがなければTAGSファイルを作成してくれる。
+(defadvice find-tag (before c-tag-file activate)
+  "Automatically create tags file."
+  (let ((tag-file (concat default-directory "TAGS")))
+    (unless (file-exists-p tag-file)
+      (shell-command "etags *.[ch] *.el .*.el -o TAGS 2>/dev/null"))
+    (visit-tags-table tag-file)))
+
+
+;;
+;;flymake
+;;
+(require 'flymake)
+(require 'popup)
+(add-hook 'c-mode-common-hook '(lambda () (flymake-mode t)))
+(defun my-flymake-popup-error ()
+  (interactive)
+  (popup-tip (mapconcat '(lambda (line-err-info)
+			   (format "[%s] %s"
+				   (flymake-ler-line line-err-info)
+				   (flymake-ler-text line-err-info)))
+			(car (flymake-find-err-info flymake-err-info
+						    (flymake-current-line-no)))
+			"\n")))
+(global-set-key (kbd "C-c SPC") 'my-flymake-popup-error)
+;flymake時警告は青色にする
+(defun flymake-parse-line (line)
+  "Parse LINE to see if it is an error or warning.
+Return its components if so, nil otherwise."
+  (let ((raw-file-name nil)
+	(line-no 0)
+	(err-type "e")
+	(err-text nil)
+	(patterns flymake-err-line-patterns)
+	(matched nil))
+    (while (and patterns (not matched))
+      (when (string-match (car (car patterns)) line)
+	(let* ((file-idx (nth 1 (car patterns)))
+	       (line-idx (nth 2 (car patterns))))
+
+	  (setq raw-file-name (if file-idx (match-string file-idx line) nil))
+	  (setq line-no       (if line-idx (string-to-number (match-string line-idx line)) 0))
+	  (setq err-text      (if (> (length (car patterns)) 4)
+				  (match-string (nth 4 (car patterns)) line)
+				(flymake-patch-err-text (substring line (match-end 0)))))
+	  (or err-text (setq err-text "<no error text>"))
+	  (if (and err-text (string-match "[wW]arning\\|警告" err-text))
+	      (setq err-type "w")
+	    )
+	  (flymake-log 3 "parse line: file-idx=%s line-idx=%s file=%s line=%s text=%s" file-idx line-idx
+		       raw-file-name line-no err-text)
+	  (setq matched t)))
+      (setq patterns (cdr patterns)))
+    (if matched
+	(flymake-ler-make-ler raw-file-name line-no err-type err-text)
+      ())))
+;;; for ruby-mode
+;; flymake-ruby
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+         (local-file (file-relative-name temp-file
+					 (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.rb$" flymake-ruby-init))
+;; (add-to-list 'flymake-err-line-patterns '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3))
+(add-hook 'ruby-mode-hook '(lambda ()
+			     (if (not (null buffer-file-name))
+				 (flymake-mode t))))
+
+;;
+;; Evernote-mode
+;;
+(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; optional
+(require 'evernote-mode)
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
+(global-set-key "\C-cep" 'evernote-post-region)
+(global-set-key "\C-ceb" 'evernote-browser)
+
+
+;;
+;; auto-complete-mode
+;;
+;; (add-to-list 'load-path ac-dir)
+(require 'auto-complete)
+(require 'auto-complete-config)    ; 必須ではないですが一応
+(add-to-list 'ac-dictionary-directories "~/elisp/auto-complete-1.3.1/dict/")
+(global-auto-complete-mode t)
+(setq ac-auto-start nil)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+;;(define-key ac-completing-map (kbd “TAB”) ‘ac-complete)
+(setq ac-dwim t)
+;; autocomplete
+
+
+;;
+;;howm
+;; 	
+(setq howm-menu-lang 'ja)
+(global-set-key "\C-c,," 'howm-menu)
+(mapc
+ (lambda (f)
+   (autoload f
+     "howm" "Hitori Otegaru Wiki Modoki" t))
+ '(howm-menu howm-list-all howm-list-recent
+             howm-list-grep howm-create
+             howm-keyword-to-kill-ring)) 	
+;; リンクを TAB で辿る
+(eval-after-load "howm-mode"
+  '(progn
+     (define-key howm-mode-map [tab] 'action-lock-goto-next-link)
+     (define-key howm-mode-map [(meta tab)] 'action-lock-goto-previous-link)))
+;; 「最近のメモ」一覧時にタイトル表示
+(setq howm-list-recent-title t)
+;; 全メモ一覧時にタイトル表示
+(setq howm-list-all-title t)
+;; メニューを 2 時間キャッシュ
+(setq howm-menu-expiry-hours 2)
+;; howm の時は auto-fill で
+(add-hook 'howm-mode-on-hook 'auto-fill-mode)
+;; RET でファイルを開く際, 一覧バッファを消す
+;; C-u RET なら残る
+(setq howm-view-summary-persistent nil)
+;; メニューの予定表の表示範囲
+;; 10 日前から
+(setq howm-menu-schedule-days-before 10)
+;; 3 日後まで
+(setq howm-menu-schedule-days 3)
+;; howm のファイル名
+;; 以下のスタイルのうちどれかを選んでください
+;; で，不要な行は削除してください
+;; 1 メモ 1 ファイル (デフォルト)
+(setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.howm")
+(setq howm-view-grep-parse-line
+      "^\\(\\([a-zA-Z]:/\\)?[^:]*\\.howm\\):\\([0-9]*\\):\\(.*\\)$")
+;; 検索しないファイルの正規表現
+(setq
+ howm-excluded-file-regexp
+ "/\\.#\\|[~#]$\\|\\.bak$\\|/CVS/\\|\\.doc$\\|\\.pdf$\\|\\.ppt$\\|\\.xls$")
+;; いちいち消すのも面倒なので
+;; 内容が 0 ならファイルごと削除する
+(if (not (memq 'delete-file-if-no-contents after-save-hook))
+    (setq after-save-hook
+          (cons 'delete-file-if-no-contents after-save-hook)))
+(defun delete-file-if-no-contents ()
+  (when (and
+         (buffer-file-name (current-buffer))
+         (string-match "\\.howm" (buffer-file-name (current-buffer)))
+         (= (point-min) (point-max)))
+    (delete-file
+     (buffer-file-name (current-buffer)))))
+;; http://howm.sourceforge.jp/cgi-bin/hiki/hiki.cgi?SaveAndKillBuffer
+;; C-cC-c で保存してバッファをキルする
+(defun my-save-and-kill-buffer ()
+  (interactive)
+  (when (and
+         (buffer-file-name)
+         (string-match "\\.howm"
+                       (buffer-file-name)))
+    (save-buffer)
+    (kill-buffer nil)))
+(eval-after-load "howm"
+  '(progn
+     (define-key howm-mode-map
+       "\C-c\C-c" 'my-save-and-kill-buffer)))
+ ;; メニューを自動更新しない
+(setq howm-menu-refresh-after-save nil)
+;; 下線を引き直さない
+(setq howm-refresh-after-save nil)
---
+++ shell/zshrc
@@ -0,0 +1,316 @@
+setopt prompt_subst
+export PAGER="less"
+export LESS="-imqMXR -x 8"
+export LESSCHARSET=utf-8
+export EDITOR="vi"
+
+export TEXMFHOME=~/texmf	# dont set TEXINPUTS
+export BIBCITEDIR=${TEXMFHOME}/bibtex/bib
+
+export CVSROOT=/project/camp/cvs # solaris user
+#export CVSROOT=camp:/project/camp/cvs  # for camp group
+#export CVS_RSH=ssh
+
+#
+# aliases
+#
+
+#alias ls='ls -F'
+alias la='ls -A'
+alias ll='ls -l'
+alias lla='ls -l -A'
+alias du='du -hk'
+#alias grep='grep --color'
+#alias egrep='egrep --color'
+
+set -o noclobber		# no overwrite when redirect
+
+# user file-creation mask
+umask 022
+
+#
+# zsh gadgets
+#
+autoload -U compinit
+compinit
+
+# autoload predict-on
+# predict-on
+
+bindkey -e
+bindkey ' ' magic-space
+
+setopt always_last_prompt
+setopt auto_cd
+setopt auto_list
+setopt auto_param_slash
+setopt auto_pushd
+setopt auto_remove_slash
+setopt auto_resume
+setopt bad_pattern
+setopt beep
+#setopt nobeep
+setopt brace_ccl
+setopt pushd_silent
+setopt complete_aliases
+setopt complete_in_word
+setopt correct_all
+setopt csh_null_glob
+setopt extended_glob
+unsetopt flow_control           # disable stop=^s and start=^q
+setopt glob_dots
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt interactive_comments
+setopt list_ambiguous
+setopt list_types
+setopt path_dirs
+setopt pushd_ignore_dups
+setopt pushd_to_home
+setopt short_loops
+setopt sun_keyboard_hack
+setopt rc_expand_param
+
+limit -h coredumpsize 0
+
+#
+# PATH
+#
+case $OSTYPE in
+linux-gnu*)
+    case $HOST in
+    moco.matlab.nitech.ac.jp)
+    PROMPT="%n@"$'%{\e[$[44]m%}'"%m"$'\e[m'":%~/
+$ "	
+    ;;
+
+    *)
+    PROMPT="%n@"$'%{\e[$[42]m%}'"%m"$'\e[m'":%~/
+$ "
+    ;;
+    esac
+
+    export PATH=/bin:/usr/bin:/usr/X11R6/bin:/usr/local/bin:/usr/local/ruby/bin:/project/camp/sim/proj-sap/bin
+    echo -n '[31m'
+    /usr/bin/quota -q
+    echo -n '[00m'
+    
+    # ls color
+    alias ls='ls -F --color'
+    ;;
+
+solaris*)
+#	PROMPT=$'%{\e[$[41]m%}'"%n@%m"$'\e[m'":%~/$ "
+    PROMPT="%n@"$'%{\e[$[41]m%}'"$HOST"$'\e[m'":%~/
+$ "
+	
+    export LANG='ja'
+    export MANPATH=/usr/man:/opt/local/man:/usr/sfw/man:/opt/csw/man:/opt/SUNWspro/man:/usr/openwin/man:/usr/dt/man:/usr/X11/man
+    export PATH=/opt/local/bin:/usr/bin:/usr/sbin:/usr/sfw/bin:/opt/csw/bin:/usr/ccs/bin:/opt/SUNWspro/bin:/usr/openwin/bin:/usr/dt/bin:/usr/X11/bin:/project/camp/bin:/project/camp/sim/proj-sap/bin
+    noglob stty erase ^h
+    noglob stty intr  ^c
+    noglob stty susp  ^z
+    echo -n '[31m'
+    /usr/sbin/quota
+    echo -n '[00m'
+
+    # ls color
+    case $HOST in
+	sangria)
+	    alias ls='ls -F'
+	    ;;
+	*)
+	    alias ls='gls -F --color'
+	    ;;
+    esac
+    #eval `dircolors ~/.dir_colors`
+    #source ~/.lscolor
+    zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+    ;;
+
+freebsd*)#FreeBSD用
+#	PROMPT=$'%{\e[$[41]m%}'"%n@%m"$'\e[m'":%~/$ "
+    PROMPT="%n@"$'%{\e[$[46]m%}'"%m"$'\e[m'":%~/
+$ "	
+	
+    export LANG='ja'
+#    export MANPATH=/usr/man:/opt/local/man:/usr/sfw/man:/opt/csw/man:/opt/SUNWspro/man:/usr/openwin/man:/usr/dt/man:/usr/X11/man
+ #   export PATH=/opt/local/bin:/usr/bin:/usr/sbin:/usr/sfw/bin:/opt/csw/bin:/usr/ccs/bin:/opt/SUNWspro/bin:/usr/openwin/bin:/usr/dt/bin:/usr/X11/bin:/project/camp/bin:/project/camp/sim/proj-sap/bin
+
+    export PATH=/bin:/usr/bin:/usr/X11R6/bin:/usr/local/bin:/usr/local/ruby/bin:/project/camp/sim/proj-sap/bin
+    echo -n '[31m'
+
+    echo -n '[00m'
+
+    # ls color
+#    alias ls='ls -F --color'
+    alias ls='ls -FG'
+    export LSCOLORS=ExGxxxxxCxxxxxxxxxxxxx
+    #eval `dircolors ~/.dir_colors`
+    #source ~/.lscolor
+    zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+    ;;
+esac
+
+#=========================================================
+# Customise
+#=========================================================
+
+#
+# Alias
+#
+
+#alias asm='asm a.asm | sr'
+#alias rmparent='rm ~/.mozilla/firefox/yan07onf.default/.parentlock'
+
+alias rmast='rm *~'
+alias rmdast='rm .*~'
+
+alias cp='nocorrect cp -ir'
+alias grep='nocorrect grep'
+alias ln='nocorrect ln'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv -i'
+alias setus='setxkbmap us'
+
+#alias emacs='emacs23'
+#alias emacsc='emacsclient23 -t'
+
+alias viz='vi ~/.zshrc'
+alias emz='emacs ~/.zshrc'
+alias emcz='emacsc ~/.zshrc'
+alias setz='source ~/.zshrc'
+
+alias rdesktop='rdesktop -a 16 -g 1268x940'
+alias uttsc='/opt/SUNWuttsc/bin/uttsc -g 1268x940'
+alias dccs='/opt/SUNWuttsc/bin/uttsc -g 1200x900 cs-dc.cs.nitech.ac.jp'
+
+alias dvipdf='dvipdfmx -d 5'
+
+alias -g l='| $PAGER'
+#alias -g g='| grep'
+
+alias -g ...='../..'
+alias -g ....='../../..'
+
+
+#alias emacs='TERM=xterm-256color emacs'
+
+#alias xine='xine --auto-play --auto-scan dvd'
+
+#
+# PATH
+#
+#export PATH=/usr/local/teTeX/bin:$PATH    # sh/bash ユーザの場合
+export PATH=/opt/openoffice.org3/program:$PATH
+#export PATH=/home/ikegaya/b4/compiler/p1/bin:$PATH
+
+#
+# terminal command overlap
+#
+export HISTCONTROL=ignoreboth
+
+
+#
+# zstyles
+#
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' menu select=1
+#
+# for command-line(C-w)
+#
+WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+#
+# compdef
+#
+compdef _tex platex
+compdef _dvi dvipdf
+compdef _hosts rdesktop
+zstyle ':completion:*:rdesktop:*' hosts andes onion basilic parsil aconcagua everest
+zstyle ':completion:*:rdesktop:*' sort false
+
+export LS_COLORS='di=01;34'
+
+
+#
+#キーボード設定
+#
+#setxkbmap us
+#xmodmap -eps G 64
+#xmodmap -e 'keycode 64 = Alt_L Meta_L'
+#xmodmap -eps G 64
+
+#PATH=$PATH:~yasui/bin/
+#itazura
+#alias ls="itazura-ls"
+#alias cd="echo 'rm -rf /'"
+
+alias gitgraph="git log --graph --all --color  --pretty='%x09%h %cn%x09%s %Cred%d%Creset'"
+
+# autoload -Uz vcs_info
+# zstyle ':vcs_info:*' enable git
+# autoload -Uz is-at-least
+# if is-at-least 4.3.10; then
+#     zstyle ':vcs_info:*' check-for-changes true
+#     zstyle ':vcs_info:*' formats '%R' '%S' '%b' '%s' '%c' '%u'
+#     zstyle ':vcs_info:*' actionformats '%R' '%S' '%b|%a' '%s' '%c' '%u'
+# fi
+
+# # zshのPTOMPTに渡す文字列は、可読性がそんなに良くなくて、読み書きしたり、つまりデバッグが
+# # 大変なため、文字列を組み立てるのは関数でやることにする。
+# # そのほうが分岐などを追加するのが楽。
+# # この先、追加で表示させたい情報はいろいろでてくるとおもうし。
+
+# autoload -Uz vcs_info
+# zstyle ':vcs_info:*' enable git 
+
+# # 下のformatsの値をそれぞれの変数に入れてくれる機能の、変数の数の最大。
+# # デフォルトだと2くらいなので、指定しておかないと、下のformatsがほぼ動かない。
+# zstyle ':vcs_info:*' max-exports 7
+
+# # 左から順番に、vcs_info_msg_{n}_ という名前の変数に格納されるので、下で利用する
+# zstyle ':vcs_info:*' formats '%R' '%S' '%b' '%s'
+# # 状態が特殊な場合のformats
+# zstyle ':vcs_info:*' actionformats '%R' '%S' '%b|%a' '%s'
+
+# # 4.3.10以上の場合は、check-for-changesという機能を使う。
+# function echo_rprompt () {
+#     local repos branch st color
+
+#     STY= LANG=en_US.UTF-8 vcs_info
+#     if [[ -n "$vcs_info_msg_1_" ]]; then
+#         # -Dつけて、~とかに変換
+#         repos=`print -nD "$vcs_info_msg_0_"`
+
+#         # if [[ -n "$vcs_info_msg_2_" ]]; then
+#             branch="$vcs_info_msg_2_"
+#         # else
+#         #     branch=$(basename "`git symbolic-ref HEAD 2> /dev/null`")
+#         # fi
+
+#         if [[ -n "$vcs_info_msg_4_" ]]; then # staged
+#             branch="%F{green}$branch%f"
+#         elif [[ -n "$vcs_info_msg_5_" ]]; then # unstaged
+#             branch="%F{red}$branch%f"
+#         else
+#             branch="%F{blue}$branch%f"
+#         fi
+
+#         print -n "[%25<..<"
+#         print -n "%F{yellow}$vcs_info_msg_1_%F"
+#         print -n "%<<]"
+
+#         print -n "[%15<..<"
+#         print -nD "%F{yellow}$repos%f"
+#         print -n "@$branch"
+#         print -n "%<<]"
+
+#     else
+#         print -nD "[%F{yellow}%60<..<%~%<<%f]"
+#     fi
+# }
+
+# setopt prompt_subst
+# RPROMPT='`echo_rprompt`'
