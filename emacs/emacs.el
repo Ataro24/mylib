08fbb4e9a346b7c737b2ff72b3b943f95ae6daa3
tatsu <tatsu@fastener.(none)>
2013-02-14 13:30:03 +0900

    org-modeを追加

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -507,3 +507,13 @@ Return its components if so, nil otherwise."
 (setq howm-menu-refresh-after-save nil)
 ;; 下線を引き直さない
 (setq howm-refresh-after-save nil)
+;;=================== howmここまで ============================
+
+
+;;
+;; org-mode
+;;
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(add-hook 'org-mode-hook 'turn-on-font-lock);org-modeのバッファはfont-lock-mode
